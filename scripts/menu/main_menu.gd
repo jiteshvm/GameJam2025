773f253c17bf2146671889d33237c4d87d4c4fb5
Sam Quist <skelegator@gmail.com>
2025-11-28 14:18:35 -0700

    Added graphics to main menu

--- scripts/menu/main_menu.gd
+++ scripts/menu/main_menu.gd
@@ -15,9 +15,9 @@ func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Main.tscn")
 
 
-func _on_options_pressed() -> void:
+func _on_settings_pressed() -> void:
 	pass # Replace with function body.
 
 
-func _on_quit_pressed() -> void:
-	get_tree().quit()
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
