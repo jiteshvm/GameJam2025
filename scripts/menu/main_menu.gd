67e275ef90f1154a15843ed856832c0bf013cf53
Sam Quist <skelegator@gmail.com>
2025-11-29 10:40:44 -0700

    imported font and tried implementing credits button, but didn't quite make it lol

--- scripts/menu/main_menu.gd
+++ scripts/menu/main_menu.gd
@@ -4,7 +4,7 @@ var button_type = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -17,10 +17,10 @@ func _on_start_pressed() -> void:
 	$fade_transition/AnimationPlayer.play("fadeout")
 
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
+	pass
 
 func _on_credits_pressed() -> void:
-	pass # Replace with function body.
+	pass
 
 func _on_fade_timer_timeout() -> void:
 	if button_type == "start" :
