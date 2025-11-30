9e04ea58432b82da7e15ef922bdfb05924b8aca6
Sam Quist <skelegator@gmail.com>
2025-11-30 16:58:03 -0700

    controls button instead of settings button on main menu

--- scripts/menu/main_menu.gd
+++ scripts/menu/main_menu.gd
@@ -13,6 +13,7 @@ func _process(delta: float) -> void:
 func _on_start_pressed() -> void:
 	button_type = "start"
 	$CreditsText.hide()
+	$ControlsText.hide()
 	$Logo1.show()
 	$fade_transition.show()
 	$fade_transition/fade_timer.start()
@@ -20,11 +21,14 @@ func _on_start_pressed() -> void:
 
 func _on_credits_pressed() -> void:
 	$Logo1.hide()
+	$ControlsText.hide()
 	$CreditsText.show()
-
-func _on_settings_pressed() -> void:
-	pass
 
 func _on_fade_timer_timeout() -> void:
 	if button_type == "start" :
 		get_tree().change_scene_to_file("res://scenes/Main.tscn")
+
+func _on_controls_pressed() -> void:
+	$Logo1.hide()
+	$CreditsText.hide()
+	$ControlsText.show()
